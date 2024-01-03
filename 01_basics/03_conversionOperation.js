c417364f3cea01987121c13f8c18e16f11acb829
Rohan-Khedekar <147737759+Rohan-Khedekar@users.noreply.github.com>
2024-01-03 06:55:41 +0000

    conversion and operations in javascript

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -16,3 +16,36 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+// ***********************************  Operations  ********************************************
+
+console.log(3 + 2)
+console.log(3 - 2)
+console.log(3 * 2)
+console.log(3 / 2)
+console.log(3 ** 2)
+console.log(3 % 2)
+
+let str1 = "hello "
+let str2 = " Rohan"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log("1" + "2")
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+
+console.log(+true)
+console.log(+"")
+
+let x = 3
+let y = x++
+console.log(x)
+console.log(y)
+
+let p = 4
+let q = ++p
+console.log(p)
+console.log(q)
