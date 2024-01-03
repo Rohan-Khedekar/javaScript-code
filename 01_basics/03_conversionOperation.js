a7131c92149f82a79600511a274ea60d04ab9bbf
Rohan-Khedekar <147737759+Rohan-Khedekar@users.noreply.github.com>
2024-01-03 06:21:48 +0000

    conversion of datatype in javascript

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score =11
+console.log(typeof score)
+
+score ="333"
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
