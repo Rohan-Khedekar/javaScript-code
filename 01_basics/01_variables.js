dc36f958cca2f2d81ecafee50bec98b2900ff7a5
Rohan-Khedekar <147737759+Rohan-Khedekar@users.noreply.github.com>
2024-01-03 04:38:58 +0000

    variables in javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1324564
+let accountEmail = "rohan@gmail.com"
+var accountPasword = "12345"
+accountCity = "Delhi"
+
+// accountId = 3246 // not allowed
+
+accountEmail = "rk@rk.com"
+accountPasword = 432
+accountCity = "jaipur"
+let accountState;
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPasword, accountCity, accountState])
