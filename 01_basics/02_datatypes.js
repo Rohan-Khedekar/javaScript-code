d8f1355b01d45554a8677edd899f53b756c24576
Rohan-Khedekar <147737759+Rohan-Khedekar@users.noreply.github.com>
2024-01-03 05:09:29 +0000

    datatypes in javascript

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+// alert(5 + 2) 
+
+/*
+  datatypes in javascript
+  number : 2 to power 53
+  bigInt
+  string
+  boolean: true/false
+  null : standalone value
+  undefined : value not assigned
+  object
+*/
+
+let num = 12345
+let num2 = 375843546567658686n
+let name = "rohan"
+let isLoggedIn = true
+let state
+
+console.log(typeof num )       // number
+console.log(typeof num2)       // bigInt
+console.log(typeof name)       // string
+console.log(typeof isLoggedIn) // boolean
+console.log(typeof state)      // undefined
+console.log(typeof undefined)  // undefined
+console.log(typeof null)       // object
